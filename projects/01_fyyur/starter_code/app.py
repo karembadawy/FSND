2baa41e14fbf858eeb3450cff5dba5b59e00cfcf
Kareem Badawy <karem.badawy@gmail.com>
2020-08-26 22:05:29 +0200

    list shows and fix past and upcomming shows.

--- projects/01_fyyur/starter_code/app.py
+++ projects/01_fyyur/starter_code/app.py
@@ -197,7 +197,7 @@ def show_venue(venue_id):
       body['artist_id'] = show.artist_id
       body['artist_name'] = show.artist.name
       body['artist_image_link'] = show.artist.image_link
-      body['start_date'] = show.start_date.strftime(" % Y-%m-%d % H: % M: % S")
+      body['start_date'] = show.start_date.strftime('%Y-%m-%d %H:%M:%S')
       # append show data to the array
       upcomingData.append(body)
 
@@ -212,7 +212,7 @@ def show_venue(venue_id):
       body['artist_id'] = show.artist_id
       body['artist_name'] = show.artist.name
       body['artist_image_link'] = show.artist.image_link
-      body['start_date'] = show.start_date.strftime(" % Y-%m-%d % H: % M: % S")
+      body['start_date'] = show.start_date.strftime('%Y-%m-%d %H:%M:%S')
       # append show data to the array
       pastData.append(body)
 
@@ -467,7 +467,7 @@ def show_artist(artist_id):
       body['venue_id'] = show.venue_id
       body['venue_name'] = show.venue.name
       body['venue_image_link'] = show.venue.image_link
-      body['start_date'] = show.start_date.strftime(" % Y-%m-%d % H: % M: % S")
+      body['start_date'] = show.start_date.strftime('%Y-%m-%d %H:%M:%S')
       # append show data to the array
       upcomingData.append(body)
 
@@ -482,7 +482,7 @@ def show_artist(artist_id):
       body['venue_id'] = show.venue_id
       body['venue_name'] = show.venue.name
       body['venue_image_link'] = show.venue.image_link
-      body['start_date'] = show.start_date.strftime(" % Y-%m-%d % H: % M: % S")
+      body['start_date'] = show.start_date.strftime('%Y-%m-%d %H:%M:%S')
       # append show data to the array
       pastData.append(body)
 
@@ -650,45 +650,23 @@ def edit_artist_submission(artist_id):
 
 @app.route('/shows')
 def shows():
-  # displays list of shows at /shows
-  # TODO: replace with real venues data.
-  #       num_shows should be aggregated based on number of upcoming shows per venue.
-  data=[{
-    "venue_id": 1,
-    "venue_name": "The Musical Hop",
-    "artist_id": 4,
-    "artist_name": "Guns N Petals",
-    "artist_image_link": "https://images.unsplash.com/photo-1549213783-8284d0336c4f?ixlib=rb-1.2.1&ixid=eyJhcHBfaWQiOjEyMDd9&auto=format&fit=crop&w=300&q=80",
-    "start_time": "2019-05-21T21:30:00.000Z"
-  }, {
-    "venue_id": 3,
-    "venue_name": "Park Square Live Music & Coffee",
-    "artist_id": 5,
-    "artist_name": "Matt Quevedo",
-    "artist_image_link": "https://images.unsplash.com/photo-1495223153807-b916f75de8c5?ixlib=rb-1.2.1&ixid=eyJhcHBfaWQiOjEyMDd9&auto=format&fit=crop&w=334&q=80",
-    "start_time": "2019-06-15T23:00:00.000Z"
-  }, {
-    "venue_id": 3,
-    "venue_name": "Park Square Live Music & Coffee",
-    "artist_id": 6,
-    "artist_name": "The Wild Sax Band",
-    "artist_image_link": "https://images.unsplash.com/photo-1558369981-f9ca78462e61?ixlib=rb-1.2.1&ixid=eyJhcHBfaWQiOjEyMDd9&auto=format&fit=crop&w=794&q=80",
-    "start_time": "2035-04-01T20:00:00.000Z"
-  }, {
-    "venue_id": 3,
-    "venue_name": "Park Square Live Music & Coffee",
-    "artist_id": 6,
-    "artist_name": "The Wild Sax Band",
-    "artist_image_link": "https://images.unsplash.com/photo-1558369981-f9ca78462e61?ixlib=rb-1.2.1&ixid=eyJhcHBfaWQiOjEyMDd9&auto=format&fit=crop&w=794&q=80",
-    "start_time": "2035-04-08T20:00:00.000Z"
-  }, {
-    "venue_id": 3,
-    "venue_name": "Park Square Live Music & Coffee",
-    "artist_id": 6,
-    "artist_name": "The Wild Sax Band",
-    "artist_image_link": "https://images.unsplash.com/photo-1558369981-f9ca78462e61?ixlib=rb-1.2.1&ixid=eyJhcHBfaWQiOjEyMDd9&auto=format&fit=crop&w=794&q=80",
-    "start_time": "2035-04-15T20:00:00.000Z"
-  }]
+  data = []  # empty array to store all the data we need
+  # query to retreive all shows
+  shows = Show.query.join(Venue).join(Artist).all()
+
+  for show in shows:
+    body = {}  # empty object to store show data
+    # append venue data to the array
+    body['artist_id'] = show.artist_id
+    body['artist_name'] = show.artist.name
+    body['artist_image_link'] = show.artist.image_link
+    body['venue_id'] = show.venue_id
+    body['venue_name'] = show.venue.name
+    body['start_date'] = show.start_date.strftime('%Y-%m-%d %H:%M:%S')
+
+    # append artist data to the array
+    data.append(body)
+
   return render_template('pages/shows.html', shows=data)
 
 #  Create Shows
