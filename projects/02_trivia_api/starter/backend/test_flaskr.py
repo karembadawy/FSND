cf0d42f87437a2341ef2f28075982a7a6af65707
Kareem Badawy <karem.badawy@gmail.com>
2020-09-16 23:03:32 +0200

    trivia_api: test Categories.

--- projects/02_trivia_api/starter/backend/test_flaskr.py
+++ projects/02_trivia_api/starter/backend/test_flaskr.py
@@ -8,33 +8,48 @@ from models import setup_db, Question, Category
 
 
 class TriviaTestCase(unittest.TestCase):
-    """This class represents the trivia test case"""
+  """This class represents the trivia test case"""
 
-    def setUp(self):
-        """Define test variables and initialize app."""
-        self.app = create_app()
-        self.client = self.app.test_client
-        self.database_name = "trivia_test"
-        self.database_path = "postgres://{}/{}".format('localhost:5432', self.database_name)
-        setup_db(self.app, self.database_path)
+  def setUp(self):
+    """Define test variables and initialize app."""
+    self.app = create_app()
+    self.client = self.app.test_client
+    self.database_path = "postgresql://postgres:postgres@localhost:5432/trivia_test"
+    setup_db(self.app, self.database_path)
 
-        # binds the app to the current context
-        with self.app.app_context():
-            self.db = SQLAlchemy()
-            self.db.init_app(self.app)
-            # create all tables
-            self.db.create_all()
-    
-    def tearDown(self):
-        """Executed after reach test"""
-        pass
+    # binds the app to the current context
+    with self.app.app_context():
+      self.db = SQLAlchemy()
+      self.db.init_app(self.app)
+      # create all tables
+      self.db.create_all()
 
-    """
-    TODO
-    Write at least one test for each test for successful operation and for expected errors.
-    """
+  def tearDown(self):
+    """Executed after reach test"""
+    pass
 
+  # test Categories list
+  def test_categories(self):
+    # make request
+    request = self.client().get('/categories')
+    data = json.loads(request.data)
+
+    # test results
+    self.assertEqual(data['success'], True)
+    self.assertTrue(len(data['categories']))
+
+  # test Questions list based on category
+  def test_questions_category(self):
+    # make request
+    request = self.client().get('/categories/1/questions')
+    data = json.loads(request.data)
+
+    # test results
+    self.assertEqual(data['success'], True)
+    self.assertTrue(len(data['questions']))
+    self.assertTrue(data['total_questions'])
+    self.assertTrue(data['current_category'])
 
 # Make the tests conveniently executable
 if __name__ == "__main__":
-    unittest.main()
+  unittest.main()
